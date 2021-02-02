827e3fe2cabbdd880f99875cbe155a5aa6189c5a
victorhuruban <victor.huruban@yahoo.com>
2021-02-02 20:53:54 +0000

    Testing v0.2

--- src/com/chessgame/ServerClient/Server.java
+++ src/com/chessgame/ServerClient/Server.java
@@ -71,8 +71,8 @@ public class Server implements Runnable {
                 if (game.getMovedPiece()) {
                     try {
                         System.out.println(timer.toString());
-                        //Object[] send = { game.getChessBoard() };
-                        out.writeObject(game.getChessBoard());
+                        Object[] send = { game.getChessBoard() };
+                        out.writeObject(send);
                         game.changeMovedPiece();
                         boolean tru = true;
                         while (tru) {
@@ -84,7 +84,7 @@ public class Server implements Runnable {
                             }
                             try {
                                 System.out.println("astept ceva");
-                                ChessBoard temp = (ChessBoard)/*(ChessBoard) ((Object[]) */in.readObject();//[0];
+                                ChessBoard temp = (ChessBoard) ((Object[]) in.readObject())[0];
                                 temp.reverseBoard();
                                 game.updateChessBoardUI(temp, game.chessboard);
                                 game.chessboard.updateUI();
