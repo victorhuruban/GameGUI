67f0a0a80773853a1baa70c68f8c4874bc90dcb3
victorhuruban <victor.huruban@yahoo.com>
2021-02-08 16:55:13 +0000

    Commit for testing.

--- src/com/chessgame/Main.java
+++ src/com/chessgame/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws NullPointerException {
         GameGUI start = new GameGUI();
+        start.startServerAndHost();
+        //start.startClientAndJoin("192.168.0.155");
     }
 }
