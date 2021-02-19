5c7262614c32a446a25233741cd6d376fa98e91d
victorhuruban <victor.huruban@yahoo.com>
2021-02-19 20:21:16 +0000

    Testing lobby connection between 2 players

--- src/com/poker/ServerClient/ClientHandler.java
+++ src/com/poker/ServerClient/ClientHandler.java
@@ -45,7 +45,7 @@ public class ClientHandler implements Runnable {
                     System.out.println("acilea");
                     outToAll("Im trying to start the game");
                 } else if (req[req.length - 1].equals("2")) {
-                    System.out.println(req[0]);
+                    outToAll(conNum + " " + req[0]);
                 }
             }
         } catch (IOException e) {
