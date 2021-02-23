d22deced799db8e06fd49e24d0565c699221e467
victorhuruban <victor.huruban@yahoo.com>
2021-02-23 17:29:28 +0000

    Creating MVP for the poker game.

--- src/com/poker/Lobby/Lobby.java
+++ src/com/poker/Lobby/Lobby.java
@@ -490,7 +490,6 @@ public class Lobby {
         fold.addActionListener(e -> {
             if (turn == cons.get(0)) {
                 player.setFold();
-                interacted = true;
             }
         });
 
